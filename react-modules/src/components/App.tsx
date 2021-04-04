da3cfd84c91ccb29a0e580b04dae2f5b398a3699
irisng <irisngwl@gmail.com>
2021-04-04 21:02:28 +0800

    Add modal markup

--- react-modules/src/components/App.tsx
+++ react-modules/src/components/App.tsx
@@ -1,24 +1,34 @@
 import React, { useState } from 'react';
-import './App.scss';
-import TabsBar from './TabsBar/TabsBar';
 
-import {Tab} from './TabsBar/tab.model'
+import TabsBar from './TabsBar/TabsBar';
+import Modal from './Modal/Modal'
+
+import './App.scss';
+import { Tab } from './TabsBar/tab.model'
 import tabsBarData from './TabsBar/tabsBarData.js';
 
 
-const App:React.FC=()=> {
+const App: React.FC = () => {
 
-  const [activeTab, setActiveTab] = useState<Tab>({ ...tabsBarData[0] })
+  const [activeTab, setActiveTab] = useState<Tab>({ ...tabsBarData[0] });
+
+  const [isModalOpen, setModalStatus] = useState<boolean>(false);
+  const onModalTriggerClick = (): void => {
+    setModalStatus(true)
+  }
 
   return (
     <div className="App">
       <TabsBar
-      label="Greek Tabs"
-        tabs={tabsBarData}
-        activeTabId={activeTab.id}
-        onTabChange={(nextActiveTab: Tab):void => { setActiveTab({ ...nextActiveTab }) }}
+        label="Greek Tabs"
+        tabs={ tabsBarData }
+        activeTabId={ activeTab.id }
+        onTabChange={ (nextActiveTab: Tab): void => { setActiveTab({ ...nextActiveTab }) } }
       >
       </TabsBar>
+      <button type="button" onClick={ onModalTriggerClick }></button>
+      {isModalOpen ? <Modal></Modal> : null }
+
     </div>
   );
 }
---
+++ react-modules/src/components/Modal/Modal.tsx
@@ -0,0 +1,101 @@
+import React from 'react';
+import cx from 'classnames';
+
+interface ModalButton {
+  mode: 'close' | 'action' | 'other';
+  btnClass?: string;
+  btnType?: 'button' | 'submit' | 'reset';
+  id?: string;
+  text?: string | JSX.Element;
+  btnJSX?: JSX.Element;
+}
+
+interface ModalProps {
+  contentAlignClass?: 'modal--align-center' | 'modal--align-left' | 'modal--align-right';
+  hasTopCloseButton?: boolean;
+  topControls?: JSX.Element;
+  title?: string | JSX.Element;
+  titleClass?: string;
+  description?: string | JSX.Element;
+  buttons?: ModalButton[];
+}
+
+const Modal: React.FC<ModalProps> = props => {
+  const {
+    contentAlignClass,
+    hasTopCloseButton,
+    topControls,
+    title,
+    titleClass,
+    description,
+    children,
+    buttons
+  } = props;
+
+  const renderTopControls = (): JSX.Element | void => {
+    if (hasTopCloseButton || topControls) {
+      return (
+        <div className="modal__top-control-wrapper">
+          {hasTopCloseButton && <button className="modal__close-btn" type="button"><i className="icon icon-close"></i></button> }
+          {topControls }
+        </div>
+      );
+    }
+  }
+
+  const renderButtons = (): JSX.Element | void => {
+    if (buttons) {
+      return (
+        <div className="modal__button-wrapper">
+          {buttons.map((button: ModalButton): JSX.Element | '' => {
+            const { mode, btnClass, btnType = "button", id, text, btnJSX = '' } = button,
+              defaultBtn: JSX.Element = (
+                <button
+                  className={ cx('btn modal__btn', { 'modal__close-btn': mode === 'close' }, btnClass) }
+                  type={ btnType }
+                  id={ id }>
+                  { text }
+                </button>
+              );
+
+            if (mode === 'close' || mode === 'action') {
+              return defaultBtn;
+            }
+            return btnJSX
+
+          }
+          ) }
+        </div>
+      )
+    }
+  }
+
+  return (
+    <div className={ cx("modal", contentAlignClass) } aria-modal="true">
+      <div className="modal__overlay"></div>
+      <div className="modal__container">
+        { renderTopControls() }
+        <div className="modal__content-wrapper">
+          { title && <h2 className={ cx('modal__title', titleClass) }>{ title }</h2> }
+          { description && <p className="modal__description">{ description }</p> }
+          { children && <div className="modal__block">{ children }</div> }
+        </div>
+        { renderButtons() }
+      </div>
+
+    </div>
+  );
+}
+
+
+Modal.defaultProps = {
+  contentAlignClass: 'modal--align-left',
+  hasTopCloseButton: false,
+  topControls: undefined,
+  title: '',
+  titleClass: undefined,
+  description: '',
+  buttons: undefined
+}
+
+export default Modal;
